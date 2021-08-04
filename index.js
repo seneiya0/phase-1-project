f7a7aed9b6cf5ef0dd6b3c2c1986253ecdd73b39
Seneiya Cooke <seneiyacooke@gmail.com>
2021-08-03 20:36:36 -0400

    added search feature

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const randomAdvice = 'https://api.adviceslip.com/advice'
-
 document.addEventListener('DOMContentLoaded', () => {
     genAdvice()
-    //searchAdvice()
+    searchAdvice(event)
+    likeTheAdvice()
+    dislikeTheAdvice()
 })
 
 
@@ -17,11 +18,47 @@ adviceButton.addEventListener('click', () => {
 
 function showAdvice(advice){
     const adviceButton = document.querySelector('.button')
-    const adviceText = document.createElement('p')
+    const adviceText = document.createElement('h2')
     adviceText.textContent = `${advice.slip.advice}`
     adviceButton.append(adviceText)
 }
 
-// function searchAdvice() {
-//     const 
-// }
+function searchAdvice() {
+    const form = document.getElementById('form')
+    const search = form.querySelector('input')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        fetch(`https://api.adviceslip.com/advice/search/${search.value}`)
+        .then(r => r.json()) //console.log(r.json()))
+        .then(advice => {
+            //console.log(advice.slips)
+            advice.slips.forEach(slip => {
+                //console.log(slip)
+                const searchBar = document.querySelector('.search-container')
+                const adviceResults = document.createElement('p')
+                adviceResults.textContent = `${slip.advice}`
+                searchBar.append(adviceResults)
+            })
+        })
+    })
+    
+}
+
+
+function likeTheAdvice(){
+    let likeBtn = document.querySelector('#likebtn')
+    let input1 = document.querySelector('#input1')
+    likeBtn.addEventListener('click', ()=>{
+    input1.value = parseInt(input1.value) + 1
+    input1.style.color = "#12ff00"
+})
+}
+
+function dislikeTheAdvice(){
+    let dislikeBtn = document.querySelector('#dislikebtn')
+    let input2 = document.querySelector('#input2')
+    dislikeBtn.addEventListener('click', ()=>{
+    input2.value = parseInt(input2.value) + 1
+    input2.style.color = "#ff0000"
+})
+}
